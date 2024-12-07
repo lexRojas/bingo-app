e8ab221652617f48327c0cdaccaa122c31f82a30
lexrojas <legalcr.rojas@gmail.com>
2024-12-06 23:11:19 -0600

    bingo-app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,200 @@
-import Image from "next/image";
+'use client'
+
+import styles from '@/modules/bingo.module.css'
+import { alias } from '@/components/numeros'
+import { useRef, useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [selected, setSelected] = useState<boolean[]>(Array.from({ length: 75 }, () => false))
+  const [numeroSeleccionado, setNumeroSeleccionado] = useState<number[]>([])
+  const [numeroAlias, setNumeroAlias] = useState<number>(0)
+  const [errorNumero, setErrorNumero] = useState<boolean>(false)
+  const [valor, setValor] = useState<number>(1)
+
+
+
+
+  const numero_digitado = useRef(null)
+
+
+
+
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+    if (numero_digitado.current) {
+      const { value } = numero_digitado.current
+      const n = Number(value).valueOf()
+
+      if (e.key == 'Enter') {
+        if (n >= 1 && n <= 75) {
+          const newArray = numeroSeleccionado
+          const newArraySelected = selected
+
+          newArraySelected[n - 1] = true
+          newArray.push(Number(value).valueOf())
+
+          setNumeroSeleccionado(newArray)
+          setNumeroAlias(n)
+          setErrorNumero(false)
+          setSelected(newArraySelected)
+
+          const input: HTMLInputElement = numero_digitado.current
+
+          input.select()
+
+        } else {
+
+          setErrorNumero(true)
+        }
+
+      }
+
+    }
+  }
+
+
+  const playAgaint = () => {
+
+    const newArray: number[] = []
+    const newArraySelected: boolean[] = Array.from({ length: 75 }, () => false)
+
+    setNumeroSeleccionado(newArray)
+    setSelected(newArraySelected)
+    setNumeroAlias(0)
+    setValor(1)
+
+  }
+
+
+  const generarNumeros = (inicio: number) => {
+    return (
+      Array.from({ length: 15 }, (_, i) => (
+        <div
+          key={i + inicio}
+          id={`b${i + inicio}`}
+          className={!selected[i + (inicio - 1)] ? styles.numero : styles.numero_selected}
+        >
+          {i + inicio}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))
+
+    )
+
+  }
+
+
+
+  return (
+    <div className="container mx-auto">
+
+      <div className="flex flex-col  gap-4   md:grid grid-cols-5   md:grid-rows-[auto_auto_auto]   ">
+        <div className="flex md:col-span-3  justify-center " >
+
+          <span className='text-red-600  text-8xl font-[family-name:var(--font-bingo)] '>
+            B
+          </span>
+          <span className='text-green-500 text-8xl font-[family-name:var(--font-bingo)] '>
+            I
+          </span>
+          <span className='text-blue-950 text-8xl font-[family-name:var(--font-bingo)] '>
+            N
+          </span>
+          <span className='text-yellow-600 text-8xl font-[family-name:var(--font-bingo)] '>
+            G
+          </span>
+          <span className='text-purple-900 text-8xl font-[family-name:var(--font-bingo)] '>
+            O
+          </span>
+
+
+        </div>
+        <div className="flex items-center justify-center  md:col-span-2  md:row-span-2 md:col-start-4">
+
+          <div className={styles.bola}>
+            <p className='flex'>{numeroAlias}</p>
+          </div>
+
+
+        </div>
+        <div className="flex md:col-span-3  md:row-start-2 justify-center">
+          <div className="grid grid-cols-5 gap-4 text-center">
+            {/* Columna B */}
+            <div className="">
+              <div className="px-3 font-bold text-xl mb-2">B</div>
+              {generarNumeros(1)}
+            </div>
+            {/* Columna I */}
+            <div>
+              <div className="font-bold text-xl mb-2">I</div>
+              {generarNumeros(16)}
+            </div>
+            {/* Columna N */}
+            <div>
+              <div className="font-bold text-xl mb-2">N</div>
+              {generarNumeros(31)}
+            </div>
+            {/* Columna G */}
+            <div>
+              <div className="font-bold text-xl mb-2">G</div>
+              {generarNumeros(46)}
+            </div>
+            {/* Columna O */}
+            <div>
+              <div className="font-bold text-xl mb-2">O</div>
+              {generarNumeros(61)}
+            </div>
+          </div>
+
+
+        </div>
+        <div className="flex rounded-xl  justify-center md:col-span-2  bg-green-500 md:row-start-3">
+
+          <button className='bg-yellow-400 hover:bg-yellow-700 text-gray-800 font-semibold py-2 px-9 my-2 border border-gray-400 rounded-xl shadow '
+            onClick={playAgaint}>
+            Jugar de nuevo
+          </button>
+
+
+
+        </div>
+        <div className="flex  rounded-xl md:col-start-3 bg-yellow-300 md:row-start-3">
+
+          <div className='flex flex-col m-2 mx-auto'>
+
+            <label className="" htmlFor="numero">Numero:</label>
+            <input className='text-center'
+              type="number"
+              name="numero"
+              id="numero"
+              min={1}
+              max={75}
+              defaultValue={1}
+              ref={numero_digitado}
+              onKeyDown={handleKeyPress}
+              value={valor}
+              onChange={(e) => setValor(Number(e.target.value).valueOf())} />
+
+          </div>
+
+
+
+        </div>
+        <div className="flex   rounded-xl items-center align-middle justify-center text-2xl font-bold md:col-start-4 md:col-span-2 bg-purple-400 md:row-start-3">
+
+          {!errorNumero ? (
+            numeroAlias >= 1 ? (
+              <span> {alias.numerosBingo?.find(item => item.numero == numeroAlias)?.nombre || ""} </span>
+            ) : null
+          ) : (
+            <span className='text-red-600 text-2xl '> NUMERO NO PERMITIDO </span>
+          )
+          }
+        </div>
+      </div>
+
+
     </div>
   );
 }
